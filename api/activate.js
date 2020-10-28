bee981c3c1bf5077016a36e40d8cd4bcae92befc
ghadad <ghadad@gmail.com>
2020-10-28 14:25:07 +0200

    wip

--- api/activate.js
+++ api/activate.js
@@ -1,19 +1,21 @@
 const Axios = require("axios");
 
-let  hooks ={};
-  Object.keys(process.env).forEach(k =>{
-    if(k.match(/K\d/))
-    hooks[k] = process.env[k];
+let hooks = {};
+Object.keys(process.env).forEach(k => {
+    if (k.match(/K\d/))
+        hooks[k] = process.env[k];
 })
 
 module.exports = (req, res) => {
-    if(req.headers[X-iCount-Secret] !==  process.env.ICOUNT_SECRET ) 
-    return res.status(401).send({error:"invalid icount secret"})
+    if (req.headers['X-iCount-Secret'] !== process.env.ICOUNT_SECRET)
+        return res.status(401).send({
+            error: "invalid icount secret"
+        })
     res.json({
-     //  hooks:hooks,
-     headers:req.headers, 
-      body: req.body,
-      query: req.query,
-      cookies: req.cookies,
+        //  hooks:hooks,
+        headers: req.headers,
+        body: req.body,
+        query: req.query,
+        cookies: req.cookies,
     })
-  }
+}
