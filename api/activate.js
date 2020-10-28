730c708199c16893958e861e314620bb3c35d19c
ghadad <ghadad@gmail.com>
2020-10-28 20:01:07 +0200

    wip

--- api/activate.js
+++ api/activate.js
@@ -10,97 +10,100 @@ Object.keys(process.env).forEach(k => {
 module.exports = async (req, res) => {
     let params = req.body || {};
     console.log("get activation request %j", req.body);
-    if (params.doctype !== "invrec" ){
-        console.error("params.doctype:",params.doctype)
+    if (params.doctype !== "invrec") {
+        console.error("params.doctype:", params.doctype)
         return res.json({
             success: true,
             message: "not kajabi transaction"
         })
     }
-        
 
 
-let icountSecret = req.headers['X-iCount-Secret'] || req.headers['x-icount-secret'];
 
-let errors = [];
-if (icountSecret !== process.env.ICOUNT_SECRET){
-    console.error("icountSecret",icountSecret,process.env.ICOUNT_SECRET)
-    errors.push("invalid icount secret")
-}
-    
+    let icountSecret = req.headers['X-iCount-Secret'] || req.headers['x-icount-secret'];
 
-if (!req.body.sku)
-    errors.push("missing sku")
+    let errors = [];
+    if (icountSecret !== process.env.ICOUNT_SECRET) {
+        console.error("icountSecret", icountSecret, process.env.ICOUNT_SECRET)
+        errors.push("invalid icount secret")
+    }
 
-if (!req.body.email)
-    errors.push("missing email")
 
-if (!req.body.name)
-    errors.push("missing name")
-if (errors.length)
-    return res.status(500).send({
-        errors: errors
-    });
+    if (!req.body.sku)
+        errors.push("missing sku")
+
+    if (!req.body.email)
+        errors.push("missing email")
+
+    if (!req.body.name)
+        errors.push("missing name")
+    if (errors.length)
+        return res.status(500).send({
+            errors: errors
+        });
 
 
     let items = params.items || []
-    let item = items[0] ;
+    let item = items[0];
 
-    if(!item)
-    return res.status(500).send({
-        error:"cannot find course item"
-    });
+    if (!item)
+        return res.status(500).send({
+            error: "cannot find course item"
+        });
 
-let webhook = `https://checkout.kajabi.com/webhooks/offers/${item.sku}/activate`;
-/* if (!webhook)
-    return res.status(401).send({
-        error: "Cannot find proper webhook for sku : " + req.body.sku
-    });
-*/
+    let webhook = `https://checkout.kajabi.com/webhooks/offers/${item.sku}/activate`;
+    /* if (!webhook)
+        return res.status(401).send({
+            error: "Cannot find proper webhook for sku : " + req.body.sku
+        });
+    */
 
-let whr = {};
-let errData, errStatus, errMessage;
-try {
-    whr = await Axios.post(webhook, {
-        name: params.clientname ,
-        email: params.client.email
-    });
+    let whr = {};
+    let errData, errStatus, errMessage;
+    try {
+        console.log("webhook:", webhook);
+        let whrParams = {
+            name: params.clientname,
+            email: params.client.email
+        };
+        console.log("params:", whrParams)
+        whr = await Axios.post(webhook, whrParams);
 
-} catch (error) {
-    if (error.response) {
-        // Request made and server responded
-        errData = error.response.data;
-        errStatus = error.response.status;
-        
-    } else if (error.request) {
-        // The request was made but no response was received
-        console.log("error.request", error.request);
-    } else {
-        // Something happened in setting up the request that triggered an Error
-        errMessage = error.message;
-    }
+    } catch (error) {
+        if (error.response) {
+            // Request made and server responded
+            errData = error.response.data;
+            errStatus = error.response.status;
 
-    console.error("errData",errData)
-    console.error("errStatus",errStatus)
-    console.error("errMessage",errMessage)
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.log("error.request", error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            errMessage = error.message;
+        }
 
-};
+        console.error("errData", errData)
+        console.error("errStatus", errStatus)
+        console.error("errMessage", errMessage)
 
-/* res.json({
-    hooks:hooks,
-    headers: req.headers,
-    body: req.body,
-    query: req.query,
-    cookies: req.cookies,
-})
-*/
-console.log("whr data:%j",whr.data);
-res.json({
-    data: whr.data,
-    success: true,
-    v: 1,
-    errData: errData,
-    errStatus: errStatus,
-    errMessage: errMessage
-})
+    };
+
+    /* res.json({
+        hooks:hooks,
+        headers: req.headers,
+        body: req.body,
+        query: req.query,
+        cookies: req.cookies,
+    })
+    */
+    console.log("whr data:%j", whr.data);
+    res.json({
+        data: whr.data,
+        success: true,
+        v: 1,
+        errData: errData,
+        errStatus: errStatus,
+        errMessage: errMessage
+    })
 }
