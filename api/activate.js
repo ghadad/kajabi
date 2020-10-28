bbac16f58c1e253d039e07f7613042267d90550e
ghadad <ghadad@gmail.com>
2020-10-28 13:41:02 +0200

    wip

--- api/activate.js
+++ api/activate.js
@@ -1,7 +1,13 @@
 const Axios = require("axios");
 
+let  hooks;
+  Object.keys(process.env).forEach(k =>{
+    if(k.match(/K\d/))
+    hooks[k] = process.env[k];
+})
 module.exports = (req, res) => {
     res.json({
+      hooks:hooks,
       body: req.body,
       query: req.query,
       cookies: req.cookies,
