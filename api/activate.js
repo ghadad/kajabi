5665a0245746188d57a8e3f1c6f8934ddc5d9b58
ghadad <ghadad@gmail.com>
2020-10-28 14:34:25 +0200

    wip

--- api/activate.js
+++ api/activate.js
@@ -6,17 +6,41 @@ Object.keys(process.env).forEach(k => {
         hooks[k] = process.env[k];
 })
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
+
     if (!(req.headers['X-iCount-Secret'] == process.env.ICOUNT_SECRET || req.headers['x-icount-Secret'] == process.env.ICOUNT_SECRET))
         return res.status(401).send({
             error: "invalid icount secret"
         });
+    if(!req.body.sku) 
+    return res.status(401).send({
+        error: "missing sku"
+    });
+    if(!req.body.email) 
+    return res.status(401).send({
+        error: "missing email"
+    });
+    if(!req.body.name) 
+    return res.status(401).send({
+        error: "missing name"
+    });
 
-    res.json({
-        //  hooks:hooks,
+    let webhook =  hooks[req.body.sku] 
+    if(!webhook) 
+    return res.status(401).send({
+        error: "Cannot find proper webhook for sku : " + req.body.sku
+    });
+
+    let whr = await Axios.post(webhook,{email:req.body.email,name:req.body.name})    ;
+    console.log(whr)
+    /* res.json({
+        hooks:hooks,
         headers: req.headers,
         body: req.body,
         query: req.query,
         cookies: req.cookies,
     })
+    */
+
+    res.json({success:true})
 }
