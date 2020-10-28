62844d679b559e881e2984a0cf0086b3c0497f73
ghadad <ghadad@gmail.com>
2020-10-28 14:26:50 +0200

    wip

--- api/activate.js
+++ api/activate.js
@@ -9,7 +9,7 @@ Object.keys(process.env).forEach(k => {
 module.exports = (req, res) => {
     if (req.headers['X-iCount-Secret'] !== process.env.ICOUNT_SECRET)
         return res.status(401).send({
-            error: "invalid icount secret"
+            error: "invalid icount secret",headers:req.headers
         })
     res.json({
         //  hooks:hooks,
