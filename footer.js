41acdb5d751f2a9c44d29140a6f3673b8459cf1d
ghadad <ghadad@gmail.com>
2021-03-26 15:48:36 +0300

    wip

--- footer.js
+++ footer.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
   $("footer .link-list").each(function() {
     //console.log($(this).remove());
-    $(this).html($footerLinks[0].html)
+   // $(this).html($footerLinks[0].html)
   });
 console.log($footerLinks) ;
 });
