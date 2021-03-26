a77b7f7fd767023120d7359fc2c324ed11aab36a
ghadad <ghadad@gmail.com>
2021-03-26 23:19:17 +0300

    wip

--- footer_links.js
+++ footer_links.js
@@ -1,6 +1,16 @@
-window.$footerLinks = [{ 
+window.$footerLinks = [{
+  html:
+  '<ul class="footer_ul">'+
+   '<li><a class="link-list__link" href="https://www.difuzia.org/chemistry-biochmistry">כימיה-ביוכימיה</a></li>' + 
+   '<li>  <a class="link-list__link" href="https://www.difuzia.org/anatomy-ab">אנטומיה-פיזיולוגיה</a></li>' + 
+   '<li>  <a class="link-list__link" href="https://www.difuzia.org/microbiology-course-page">מיקרוביולוגיה</a></li>' + 
+   '<li>  <a class="link-list__link" href="https://www.difuzia.org/pharmacology">פרמקולוגיה</a></li>' + 
+   '<li>  <a class="link-list__link" href="https://www.difuzia.org/heshbon">חשבון רוקחי</a></li>' + 
+   '<li>   <a class="link-list__link" href="https://www.difuzia.org/store">לכל הקורסים</a></li>' + 
+   '</ul>'
+},{ 
      html :
-     '<ul>'+
+     '<ul class="footer_ul">'+
      '<li><a class="link-list__link" href="https://www.difuzia.org/about" >אודות האתר</a></li>'+  
      '<li><a class="link-list__link" href="https://www.difuzia.org/groups" >רכישה קבוצתית</a></li>'+
      '<li><a class="link-list__link" href="https://www.difuzia.org/037f5756-bbf3-41cb-8b6c-41e832b0674d" >מוסדות ובתי ספר</a></li>' +
