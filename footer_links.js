1d0f84b9c0f193b6a717d57c1fa30f70932bf71b
ghadad <ghadad@gmail.com>
2021-03-26 23:26:38 +0300

    wip

--- footer_links.js
+++ footer_links.js
@@ -1,4 +1,11 @@
 window.$footerLinks = [{ 
+  html :
+  '<ul class="footer_ul">'+
+  '<li><a class="link-list__link" href="https://www.difuzia.org/store">חנות הקורסים</a></li>'+  
+  '<li><a class="link-list__link" href="https://www.difuzia.org/library">הקורסים שלי</a></li>'+
+  '<li><a class="link-list__link" href="https://www.difuzia.org/sch2020">תכנית הקורסים</a></li>' +
+  '</ul>'
+},{ 
      html :
      '<ul class="footer_ul">'+
      '<li><a class="link-list__link" href="https://www.difuzia.org/about" >אודות האתר</a></li>'+  
@@ -10,12 +17,11 @@ window.$footerLinks = [{
 },{
   html:
   '<ul class="footer_ul">'+
-   '<li><a class="link-list__link" href="https://www.difuzia.org/chemistry-biochmistry">כימיה-ביוכימיה</a></li>' + 
-   '<li>  <a class="link-list__link" href="https://www.difuzia.org/anatomy-ab">אנטומיה-פיזיולוגיה</a></li>' + 
-   '<li>  <a class="link-list__link" href="https://www.difuzia.org/microbiology-course-page">מיקרוביולוגיה</a></li>' + 
-   '<li>  <a class="link-list__link" href="https://www.difuzia.org/pharmacology">פרמקולוגיה</a></li>' + 
-   '<li>  <a class="link-list__link" href="https://www.difuzia.org/heshbon">חשבון רוקחי</a></li>' + 
-   '<li>   <a class="link-list__link" href="https://www.difuzia.org/store">לכל הקורסים</a></li>' + 
+   '<li><a class="link-list__link" href="https://www.difuzia.org/contact">צור קשר</a></li>' + 
+   '<li> <a class="link-list__link" href="https://www.difuzia.org/takanon">תנאי שימוש</a></li>' + 
+   '<li> <a class="link-list__link" href="https://www.difuzia.org/faq">שאלות נפוצות</a></li>' + 
+   '<li><a class="link-list__link" href="https://www.difuzia.org/help">בעיות נפוצות</a></li>' + 
+   '<li><a class="link-list__link" href="https://www.difuzia.org/payment">התשלום באתר</a></li>' + 
    '</ul>'
 },{
   html:
