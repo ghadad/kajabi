e1e41e4ca7718c2e50d06bc287a4f9a96de9f177
Golan Hadad <ghadad@gmail.com>
2022-02-19 21:24:24 +0200

    wip

--- footer.js
+++ footer.js
@@ -10,13 +10,11 @@ $(document).ready(function () {
   
     if(affref) {
         $( "a[href*='/app.icount.co.il/'" ).on("click", function (e) {
-
-              var url = new URL($(this).attr("href"));
-              
+             var url = new URL($(this).attr("href"));
               var params = new URLSearchParams(url.search);
               params.set("ipn_url",'https://kajabi.vercel.app/api/ipn?affref=' +affref);
-              $(this).attr("href", url.pathname + '?' + params.toString()); // change link href
-              console.log("url",url.pathname + '?' + params.toString())
+              $(this).attr("href", url.origin + '?' + params.toString()); // change link href
+              console.log("url",url.origin + '?' + params.toString())
               $(this).trigger("click"); 
               return false;
         });
