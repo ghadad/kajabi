713d959f06009095b7d11ded627d72649e1efcde
root <root@DESKTOP-V6UN77N.localdomain>
2022-02-19 21:09:34 +0200

    added tapfiliate ipn

--- footer.js
+++ footer.js
@@ -1,5 +1,24 @@
 $(document).ready(function () {
 
+  var locationParams  = new URLSearchParams(location.search);
+
+  var  affref = locationParams.get("ref");
+  if(affref) {
+      $.cookie("affref", affref, { expires : 20 });
+  }
+  affref = $.cookie("affref");
+  
+    if(affref) {
+        $( "a[href*='/app.icount.co.il/'" ).on("click", function (e) {
+              var url = new URL($(this).attr("href"));
+              var params = new URLSearchParams(url.search);
+              params.set("ipn_url",'https://kajabi.vercel.app/api/ipn?affref=' +affref);
+              $(this).attr("href", url.pathname + '?' + params.toString()); // change link href
+              $(this).trigger("click"); 
+              return false;
+        });
+  }
+
   var lists = $("footer .link-list");
   if (lists.length > 1) {
     $("footer").show();
